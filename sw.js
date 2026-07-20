3936fdbbdabc2e50950d8ba30b7c91469c2fbbd9
Keatinge5418 <keatinge@hotmail.com>
2026-07-20 16:59:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "coursecalc-v111";
+const ASSETS = ["./", "./index.html", "./apple-touch-icon.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
