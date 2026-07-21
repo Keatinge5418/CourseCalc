a25a66d029c19ab47bad36c6bba64b443f91f766
Keatinge5418 <keatinge@hotmail.com>
2026-07-21 12:50:53 +0100

    Update index.html and sw.js to latest versions

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-const CACHE = "coursecalc-v117";
-const ASSETS = ["./", "./index.html", "./apple-touch-icon.png"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+/* CourseCalc Service Worker */
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('coursecalc-v1').then(cache => {
+    return cache.addAll(['/']);
+  }));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(
-    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-  )).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+  }));
 });
