2e069e1caca5d472dea95891603f17d6f5961b4f
Keatinge5418 <keatinge@hotmail.com>
2026-08-13 17:47:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "coursecalc-v379";
+const CACHE = "coursecalc-v382";
 const ASSETS = ["./", "./index.html", "./apple-touch-icon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
